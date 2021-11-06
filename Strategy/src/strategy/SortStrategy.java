012dd8d0549f1a77e5f0ffa4d427c6d2ff8779cd
์Nitipoom <Nitipoom.pure2544@gmail.com>
2021-11-06 21:34:42 +0700

    Strategy

---
+++ Strategy/src/strategy/SortStrategy.java
@@ -0,0 +1,10 @@
+package strategy;
+
+// DO NOT MODIFY ANYTHING BELOW THIS LINE!!
+import java.util.List;
+
+public interface SortStrategy {
+    void customSort(List<Person> people);
+
+    // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-COLON !!
+}
